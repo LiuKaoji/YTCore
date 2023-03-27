4ea1c9f527c92ca26f0e602a348cd216c8a94726
kaoji <617917455@qq.com>
2023-03-27 12:51:19 +0800

    更新脚本 移除无用文件

--- init.sh
+++ init.sh
@@ -1,47 +1,46 @@
 #!/bin/bash
 
-# Set the URLs for the files to download
+# 设置要下载的文件的URL
 URL1="https://github.com/LiuKaoji/YTCore/releases/download/1.0.0/Python.zip"
 URL2="https://github.com/LiuKaoji/YTCore/releases/download/1.0.0/ffmpeg.zip"
 URL3="https://github.com/LiuKaoji/YTCore/releases/download/1.0.0/env.zip"
 
-
-# Set the names of the files to download
+# 设置要下载的文件名
 FILENAME1="Python.zip"
 FILENAME2="ffmpeg.zip"
 FILENAME3="env.zip"
 
-# Set the name of the directory to extract the files to
+# 设置要提取文件的目录名
 EXTRACTDIR="YTCore"
 
-# Create the directory to extract the files to
+# 创建提取文件的目录
 mkdir -p "$EXTRACTDIR"
 
-# Download the first file from the first URL
-echo "Downloading $FILENAME1 from $URL1..."
+# 从第一个URL下载第一个文件
+echo "从 $URL1 下载 $FILENAME1 ..."
 curl -o "$FILENAME1" -L "$URL1"
 
-# Download the second file from the second URL
-echo "Downloading $FILENAME2 from $URL2..."
+# 从第二个URL下载第二个文件
+echo "从 $URL2 下载 $FILENAME2 ..."
 curl -o "$FILENAME2" -L "$URL2"
 
-# Download the third file from the third URL
-echo "Downloading $FILENAME3 from $URL3..."
+# 从第三个URL下载第三个文件
+echo "从 $URL3 下载 $FILENAME3 ..."
 curl -o "$FILENAME3" -L "$URL3"
 
-# Extract the contents of the first file to the specified directory
-echo "Extracting contents of $FILENAME1 to YTCore/Library"
+# 将第一个文件的内容提取到指定的目录
+echo "将 $FILENAME1 的内容提取到 YTCore/Library 目录"
 unzip -q -o "$FILENAME1" -d "YTCore/Library"
 
-# Extract the contents of the second file to the specified directory
-echo "Extracting contents of $FILENAME2 to /YTCore/Utility/YTVideoMuxer/DLMediaUtils"
-unzip -q -o "$FILENAME2" -d "/YTCore/Utility/YTVideoMuxer/DLMediaUtils"
-echo "Moving $FILENAME3 to YTCore/Resource/..."
+# 将第二个文件的内容提取到指定的目录
+echo "将 $FILENAME2 的内容提取到 YTCore/Utility/YTVideoMuxer/DLMediaUtils"
+unzip -q -o "$FILENAME2" -d "YTCore/Utility/YTVideoMuxer/DLMediaUtils"
+
+echo "将 $FILENAME3 移动到 YTCore/Resource 目录..."
 mv -f "$FILENAME3" "YTCore/Resource/"
 
-# Remove the downloaded files
-echo "Removing downloaded files..."
+# 删除已下载的文件
+echo "删除已下载的文件..."
 rm "$FILENAME1" "$FILENAME2"
 
-echo "Done."
-
+echo "完成."
